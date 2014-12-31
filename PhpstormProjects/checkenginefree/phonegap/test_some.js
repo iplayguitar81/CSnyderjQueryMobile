5057b9c8440519b82a9d89654fbced45751ec5a8
iplayguitar81 <iplayguitar81@yahoo.com>
2014-12-30 19:26:23 -0800

    Delete test_some.js

--- PhpstormProjects/checkenginefree/phonegap/test_some.js
+++
@@ -1,3 +0,0 @@
-
-
-var test_var='<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:inline-block;width:320px;height:100px; data-ad-client="ca-pub-4617308558434719" data-ad-slot="9450601783"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';
